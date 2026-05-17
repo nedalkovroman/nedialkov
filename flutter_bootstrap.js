49a51fec1ffa831246179493cbafafc7ee1b4c1b
nedalkovroman <nedalkovroman@gmail.com>
2026-05-18 01:52:57 +0300

    fix: 555

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "759913258" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2272582720" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
